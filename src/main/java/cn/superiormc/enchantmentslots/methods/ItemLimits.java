aeb92c764f22e903ab526a445b121a74837fee61
PQguanfang <pqguanfang@foxmail.com>
2024-07-27 17:27:45 +0800

    [1.0.1] Fix and change

--- src/main/java/cn/superiormc/enchantmentslots/methods/ItemLimits.java
+++ src/main/java/cn/superiormc/enchantmentslots/methods/ItemLimits.java
@@ -9,17 +9,17 @@ public class ItemLimits {
 
     /*
     Obtain the current maximum enchantment limit of the ItemStack object. The maximum enchantment limit is stored in the item NBT.
-    If it cannot be obtained, the default slot value is used.
+    If it cannot be found, the default slot value set here will be used.
 
-    Plugin has built-in default slot system, you can use ConfigReader.getDefaultLimits(player, itemID) method to default slot in this item.
+    Plugin has built-in default slot system, you can use ConfigReader.getDefaultLimits(player, itemID) method to get default slot in this item.
     For itemID parameter: You can first use checkValid(ItemStack item) method to parse it into String object here.
      */
-    public static int getMaxEnchantments(@NotNull ItemStack item, int defaultSlot, @NotNull ItemStack itemID) {
+    public static int getMaxEnchantments(@NotNull ItemStack item, int defaultSlot, @NotNull String itemID) {
         return 0;
     }
 
     /*
-    Similar to getMaxEnchantments method, but cannot obtain maximum enchantment limit, we will just return 0 instead of return a default value.
+    Similar to getMaxEnchantments method, but if we cannot found default enchantment limit, then we will just return 0 instead of return a default value.
      */
     public static int getRealMaxEnchantments(@NotNull ItemStack item) {
         return 0;
